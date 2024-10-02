27414a74c2b77918d55b95245caf06ad32b80121
Daniel Freire <20461343+danielfreire-dev@users.noreply.github.com>
2024-10-02 12:28:41 +0100

    now with new footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
-import "/src/style/footer.css";
+import React from "react";
+import "/src/styles/footer.css";
 
 export default function Footer() {
 	return (
 		<footer className="footer">
 			<p>
-				2024 - Made with 🎲 by
+				2024 - Made with 🤳 by
 				<a href="https://danielfreire.pages.dev"> Daniel Freire</a>
 			</p>
 		</footer>

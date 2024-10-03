1f7b0c85e22e387a9c08f03dc61e19371a302692
Daniel Freire <20461343+danielfreire-dev@users.noreply.github.com>
2024-10-03 12:32:07 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from "./components/Header";
 import Meme from "./components/Meme";
 
 import "/src/styles/style.css";
+import Footer from "./components/Footer";
 
 export default function App() {
 	return (
 		<div>
 			<Header />
 			<Meme />
+			<Footer />
 		</div>
 	);
 }
